c666f17feb5b9fe4142a91daca84c6c01b3aa482
Vladislav <v.danshov@innopolis.university>
2023-07-05 01:03:09 +0300

    use code generation for mocks

--- packages/local_storage_todos_api/test/src/local_storage_todos_api_test.dart
+++ packages/local_storage_todos_api/test/src/local_storage_todos_api_test.dart
@@ -1,26 +1,28 @@
 import 'package:local_storage_todos_api/local_storage_todos_api.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 import 'package:todo_api/todo_api.dart';
 
-class MockSharedPreferences extends Mock implements SharedPreferences {}
+@GenerateNiceMocks([MockSpec<SharedPreferences>()])
+import 'local_storage_todos_api_test.mocks.dart';
 
 void main() {
   late LocalStorageTodosApi api;
-  late MockSharedPreferences mockPrefs;
+  late SharedPreferences prefs;
 
   setUp(() {
-    mockPrefs = MockSharedPreferences();
-    api = LocalStorageTodosApi(mockPrefs);
+    prefs = MockSharedPreferences();
+    api = LocalStorageTodosApi(prefs);
   });
 
   group('getTodoList', () {
     test('getTodoList returns empty list when SharedPreferences is empty',
         () async {
-      when(mockPrefs.getString('todos')).thenReturn(null);
-      when(mockPrefs.getInt('revision')).thenReturn(null);
+      when(prefs.getString('todos')).thenReturn(null);
+      when(prefs.getInt('revision')).thenReturn(null);
 
       var (todos, revision) = await api.getTodoList();
 
@@ -30,9 +32,9 @@ void main() {
 
     test('getTodoList returns todos when SharedPreferences contains todos',
         () async {
-      when(mockPrefs.getString('todos')).thenReturn(
+      when(prefs.getString('todos')).thenReturn(
           '[{"created_at": 1688302736131,"text": "test","importance": "basic","done": true,"changed_at": 1688302736131,"last_updated_by": "device_id","id": "some_id"}]');
-      when(mockPrefs.getInt('revision')).thenReturn(1);
+      when(prefs.getInt('revision')).thenReturn(1);
 
       var (todos, revision) = await api.getTodoList();
 
@@ -50,9 +52,9 @@ void main() {
   test('getTodo returns todo when SharedPreferences contains todo', () async {
     var id = "some_id";
 
-    when(mockPrefs.getString('todos')).thenReturn(
+    when(prefs.getString('todos')).thenReturn(
         '[{"created_at": 1688302736131,"text": "test","importance": "basic","done": true,"changed_at": 1688302736131,"last_updated_by": "device_id","id": "$id"}]');
-    when(mockPrefs.getInt('revision')).thenReturn(1);
+    when(prefs.getInt('revision')).thenReturn(1);
 
     var (todo, revision) = await api.getTodo(id);
 
@@ -66,6 +68,6 @@ void main() {
   });
 
   tearDown(() {
-    clearInteractions(mockPrefs);
+    clearInteractions(prefs);
   });
 }
