2b0f054919bc2ad49227d07c1ea405c3c846fcec
Vladislav <v.danshov@innopolis.university>
2023-07-05 00:20:45 +0300

    add unit test for local storage

---
+++ packages/local_storage_todos_api/test/src/local_storage_todos_api_test.dart
@@ -0,0 +1,71 @@
+import 'package:local_storage_todos_api/local_storage_todos_api.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import 'package:todo_api/todo_api.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  late LocalStorageTodosApi api;
+  late MockSharedPreferences mockPrefs;
+
+  setUp(() {
+    mockPrefs = MockSharedPreferences();
+    api = LocalStorageTodosApi(mockPrefs);
+  });
+
+  group('getTodoList', () {
+    test('getTodoList returns empty list when SharedPreferences is empty',
+        () async {
+      when(mockPrefs.getString('todos')).thenReturn(null);
+      when(mockPrefs.getInt('revision')).thenReturn(null);
+
+      var (todos, revision) = await api.getTodoList();
+
+      expect(todos, []);
+      expect(revision, 0);
+    });
+
+    test('getTodoList returns todos when SharedPreferences contains todos',
+        () async {
+      when(mockPrefs.getString('todos')).thenReturn(
+          '[{"created_at": 1688302736131,"text": "test","importance": "basic","done": true,"changed_at": 1688302736131,"last_updated_by": "device_id","id": "some_id"}]');
+      when(mockPrefs.getInt('revision')).thenReturn(1);
+
+      var (todos, revision) = await api.getTodoList();
+
+      expect(todos.length, 1);
+      expect(todos[0].id, "some_id");
+      expect(todos[0].done, true);
+      expect(todos[0].text, "test");
+      expect(todos[0].importance, Importance.basic);
+      expect(todos[0].lastUpdatedBy, "device_id");
+
+      expect(revision, 1);
+    });
+  });
+
+  test('getTodo returns todo when SharedPreferences contains todo', () async {
+    var id = "some_id";
+
+    when(mockPrefs.getString('todos')).thenReturn(
+        '[{"created_at": 1688302736131,"text": "test","importance": "basic","done": true,"changed_at": 1688302736131,"last_updated_by": "device_id","id": "$id"}]');
+    when(mockPrefs.getInt('revision')).thenReturn(1);
+
+    var (todo, revision) = await api.getTodo(id);
+
+    expect(todo.id, id);
+    expect(todo.done, true);
+    expect(todo.text, "test");
+    expect(todo.importance, Importance.basic);
+    expect(todo.lastUpdatedBy, "device_id");
+
+    expect(revision, 1);
+  });
+
+  tearDown(() {
+    clearInteractions(mockPrefs);
+  });
+}
