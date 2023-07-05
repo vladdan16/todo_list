6a8a7d2ddf2c6d8e03f552d2865860011df62c09
Vladislav <v.danshov@innopolis.university>
2023-07-05 22:05:27 +0300

    add integration test for adding task

---
+++ integration_test/app_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:todo_list/app.dart';
+import 'package:todo_list/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('add task test', (tester) async {
+    // await tester.pumpWidget(const TodoListApp());
+    app.main();
+
+    // I need this, because I don't know how to wait app to be initialized ;)
+    await Future.delayed(const Duration(seconds: 5));
+
+    await tester.pumpAndSettle();
+
+    final Finder fab = find.byType(FloatingActionButton);
+    await tester.tap(fab);
+
+    await tester.pumpAndSettle();
+
+    final Finder textField = find.byType(TextField);
+    await tester.enterText(textField, 'New Task');
+
+    final Finder saveButton = find.widgetWithText(TextButton, 'SAVE');
+    await tester.tap(saveButton);
+
+    await tester.pumpAndSettle();
+
+    expect(find.text('New Task'), findsOneWidget);
+  });
+}
